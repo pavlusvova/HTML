e54718be23e5ba32f7762d3c7555699d7bff391d
Unknown <pavlusvova26@gmail.com>
2019-05-07 21:42:06 +0300

    new files

---
+++ HTML-helpers/App_Code/ListHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace HTMLhelpers.App_Code
+{
+    public static class ListHelper
+    {
+        public static HtmlString CreateList(this IHtmlHelper html, string[] items)
+        {
+        string result = "<ul>";
+        foreach (string item in items)
+        {
+            result += $"<li>{item}</li>";
+        }
+        result += "</ul>";
+        return new HtmlString(result);
+        }
+    }
+}
